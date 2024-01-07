b0ae806c31ec87c44ed66dafac3377e323ab8aff
Quin-Darcy <quintondarcy@csus.edu>
2024-01-07 13:59:47 -0800

    finished decrypt_gcm and now passing all unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -149,6 +149,112 @@ pub fn encrypt_gcm(plaintext: &[u8], iv: &[u8], aad: &[u8], key: &[u8], tag_len:
     (ciphertext_bytes, auth_tag_bytes)
 }
 
+pub fn decrypt_gcm(ciphertext: &[u8], iv: &[u8], aad: &[u8], auth_tag: &[u8], key: &[u8]) -> (Vec<u8>, bool) {
+    // TODO: Validate size of inputs
+
+    // Generate the hash subkey for the GHASH function by applying block cipher to zero block and convert to bitvec
+    let zero_block: Vec<u8> = vec![0_u8; 16]; // 128-bit zero block
+    let hash_subkey: BitVec = byteslice_to_bitvec(&aes_ecb_cipher(&zero_block, &key));
+
+    // Define pre-counter block
+    let precounter_block: BitVec;
+
+    if iv.len() == 12 { // Is the bit length of IV is 96?
+        // Create bitvector out of the IV
+        let mut bv1: BitVec = byteslice_to_bitvec(iv);
+
+        // Create 31-bit zero bitvector concatenated with 1 true bit 
+        let mut bv2: BitVec = bitvec![0; 32];
+        bv2.set(31, true);
+
+        // Concatenate the two bitvectors into one 128-bit bitvec
+        bv1.extend(bv2);
+
+        // Convert bitvec into vector of bytes and set j_block equal to this
+        precounter_block = bv1;
+    } else {
+        // Create bitvector out of IV
+        let mut iv_bits: BitVec = byteslice_to_bitvec(iv);
+
+        // This represents the number of padding bits that when added to IV creates a bitvec with length a multiple of 128
+        let blocksize_multiple_pad_len = (128 - (iv_bits.len() % 128)) % 128;
+
+        // Create the first pad based on blocksize_multiple_pad_len
+        let zeros_pad: BitVec = bitvec![0; blocksize_multiple_pad_len + 64];
+
+        // NOTE: We add the extra 64 since in the next step we will be adding another 64-bits for the encoded length
+        //       This additional 64 bits assures that the total length is still a multiple of 128.
+
+        // Convert 64-bit length of IV to byte vector
+        let bitlen_vec = (iv_bits.len() as u64).to_be_bytes().to_vec();
+
+        // Convert bitlen_vec into bitvec
+        let length_pad: BitVec = byteslice_to_bitvec(&bitlen_vec);
+
+        // Concatenate all three bitvectors
+        iv_bits.extend(zeros_pad);
+        iv_bits.extend(length_pad);
+
+        // Set precounter_block equal to the GHASH of the padded IV with the hash_subkey - Result has same length as input
+        precounter_block = ghash(&iv_bits, &hash_subkey);
+    }
+
+    // Convert ciphertext to bitvector
+    let ciphertext_bits: BitVec = byteslice_to_bitvec(&ciphertext);
+
+    // The 32-bit increment function is applied to the pre-counter block to produce the initial
+    // counter block for an invocation of the GCTR function on the ciphertext
+    let iniitial_counter_block = inc(&precounter_block, 32);
+
+    // Invoke the GCTR function to get the ciphertext
+    let plaintext = gctr(&iniitial_counter_block, &ciphertext_bits, key);
+
+    // Convert AAD to a bitvec
+    let mut aad_bits: BitVec = byteslice_to_bitvec(aad);
+
+    // Compute the minimum number of '0' bits, possibly none, so that the bit lengths of the resulting strings are multiples of 128
+    let ciphertext_pad_len = (128 - (ciphertext_bits.len() % 128)) % 128;
+    let aad_pad_len = (128 - (aad_bits.len() % 128)) % 128;
+
+    // Get the 64-bit lengths of AAD and the ciphertext and convert to BE byte vector
+    let aad_len_vec = (aad_bits.len() as u64).to_be_bytes().to_vec();
+    let ciphertext_len_vec = (ciphertext_bits.len() as u64).to_be_bytes().to_vec();
+
+    // Convert length byte vectors into bitvectors
+    let ciphertext_len_pad = byteslice_to_bitvec(&ciphertext_len_vec);
+    let aad_len_pad = byteslice_to_bitvec(&aad_len_vec);
+
+    // Construct new bitvec out of the AAD and ciphertext as well as zero pads and length pads
+    let aad_zeros_pad = bitvec![0; aad_pad_len];
+    let ciphertext_zeros_pad = bitvec![0; ciphertext_pad_len];
+
+    // Append each bitvec to aad_bits
+    aad_bits.extend(aad_zeros_pad);
+    aad_bits.extend(ciphertext_bits.clone());
+    aad_bits.extend(ciphertext_zeros_pad);
+    aad_bits.extend(aad_len_pad);
+    aad_bits.extend(ciphertext_len_pad);
+
+    // Compute the hash of this extended bitvec using the hash subkey
+    let s = ghash(&aad_bits, &hash_subkey);
+
+    // Extract the computed authentication tag by truncating the output of GCTR with the MSB function
+    let tag_len = auth_tag.len() * 8;
+    let computed_auth_tag = msb(&gctr(&precounter_block, &s, key), tag_len);
+
+    // Convert computed_auth_tag back to byte vector and compare
+    let computed_auth_tag_bytes = bitslice_to_bytevec(&computed_auth_tag);
+    if computed_auth_tag_bytes != auth_tag {
+        println!("Error: Invalid Authentication Tag.");
+        return (Vec::new(), false);
+    }
+
+    // Convert the plaintext to a byte vector and return
+    let plaintext_bytes = bitslice_to_bytevec(&plaintext);
+
+    (plaintext_bytes, true)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -202,17 +308,49 @@ mod tests {
         assert_eq!(ciphertext, target_ciphertext);
         assert_eq!(auth_tag, target_auth_tag);
     }
+
+    #[test]
+    fn test_decrypt_gcm1() {
+        let key_str = "feffe9928665731c6d6a8f9467308308feffe9928665731c6d6a8f9467308308";
+        let ciphertext_str = "522dc1f099567d07f47f37a32a84427d643a8cdcbfe5c0c97598a2bd2555d1aa8cb08e48590dbb3da7b08b1056828838c5f61e6393ba7a0abcc9f662";
+        let iv_str = "cafebabefacedbaddecaf888";
+        let aad_str = "feedfacedeadbeeffeedfacedeadbeefabaddad2";
+        let auth_tag_str = "76fc6ece0f4e1768cddf8853bb2d551b";
+
+        let key: Vec<u8> = hex::decode(key_str).unwrap();
+        let iv: Vec<u8> = hex::decode(iv_str).unwrap();
+        let ciphertext: Vec<u8> = hex::decode(ciphertext_str).unwrap();
+        let aad: Vec<u8> = hex::decode(aad_str).unwrap();
+        let auth_tag: Vec<u8> = hex::decode(auth_tag_str).unwrap();
+
+        let (plaintext, result) = decrypt_gcm(&ciphertext, &iv, &aad, &auth_tag, &key);
+
+        let target_plaintext_str = "d9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39";
+        let target_plaintext: Vec<u8> = hex::decode(target_plaintext_str).unwrap();
+
+        assert_eq!(plaintext, target_plaintext);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_decrypt_gcm2() {
+        let key_str = "feffe9928665731c6d6a8f9467308308feffe9928665731c6d6a8f9467308308";
+        let ciphertext_str = "522dc1f099567d07f47f37a32a84427d643a8cdcbfe5c0c97598a2bd2555d1aa8cb08e48590dbb3da7b08b1056828838c5f61e6393ba7a0abcc9f662";
+        let iv_str = "cafebabefacedbaddecaf888";
+        let aad_str = "feedfacedeadbeeffeedfacedeadbeefabaddad2";
+        let auth_tag_str = "76fc6ecf0f4e1768cddf8853bb2d551b";
+
+        let key: Vec<u8> = hex::decode(key_str).unwrap();
+        let iv: Vec<u8> = hex::decode(iv_str).unwrap();
+        let ciphertext: Vec<u8> = hex::decode(ciphertext_str).unwrap();
+        let aad: Vec<u8> = hex::decode(aad_str).unwrap();
+        let auth_tag: Vec<u8> = hex::decode(auth_tag_str).unwrap();
+
+        let (plaintext, result) = decrypt_gcm(&ciphertext, &iv, &aad, &auth_tag, &key);
+
+        let target_plaintext_str = "d9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39";
+        let target_plaintext: Vec<u8> = hex::decode(target_plaintext_str).unwrap();
+
+        assert_eq!(result, false);
+    }     
 }
-
-// fn main() {
-//     let key_str: &str = "feffe9928665731c6d6a8f9467308308feffe9928665731c6d6a8f9467308308";
-//     let plaintext_str: &str = "d9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b391aafd255";
-//     let iv_str: &str = "cafebabefacedbaddecaf888";
-
-//     let key: Vec<u8> = hex::decode(key_str).expect("failed");
-//     let plaintext: Vec<u8> = hex::decode(plaintext_str).expect("failed");
-//     let iv: Vec<u8> = hex::decode(iv_str).expect("failed");
-//     let aad: Vec<u8> = Vec::new();
-
-//     encrypt_gcm(&plaintext, &iv, &aad, &key, 128);
-// }
